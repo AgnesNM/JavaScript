1a95af8ac954da58c44fa51e669a491da7a95287
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-15 16:20:48 +0300

    variable scoping and the switch statement

--- conditionals.js
+++ conditionals.js
@@ -1,15 +1,60 @@
-let userLoggedIn = true;
-let userHasPremiumAccount = true;
-let userHasMegaPremiumAccount = false;
+// let userLoggedIn = true;
+// let welcomeMessage = "";
 
-userLoggedIn && userHasPremiumAccount;
-userLoggedIn && userHasMegaPremiumAccount;
 
-userLoggedIn || userHasPremiumAccount;
-userLoggedIn || userHasMegaPremiumAccount;
+// if (userLoggedIn){
+//     welcomeMessage = "Welcome back!";    
+// } else {
+//     let welcomeMessage = "Welcome new user!";
+// }
 
-!userLoggedIn;
-!userHasMegaPremiumAccount;
+let firstUser = {
+    name:"Will Alex",
+    age:33,
+    accountLevel:"normal"
+}
+
+
+let secondUser = {
+    name:"Sarah Kate",
+    age:21,
+    accountLevel:"premium"
+}
+
+let thirdUser = {
+    name:"Audrey Simon",
+    age:27,
+    accountLevel: "mega-premium"
+}
+
+
+switch(thirdUser.accountLevel) {
+    case "normal":
+        console.log("You have a normal account!");
+        break;
+    case "premium":
+        console.log("You have a premium account!");
+        break;
+    case "mega-premium":
+        console.log("You have a mega premium account!");
+        break;
+    default:
+        console.log("Unkown account type!");
+}
+
+
+
+// let userHasPremiumAccount = true;
+// let userHasMegaPremiumAccount = false;
+
+// userLoggedIn && userHasPremiumAccount;
+// userLoggedIn && userHasMegaPremiumAccount;
+
+// userLoggedIn || userHasPremiumAccount;
+// userLoggedIn || userHasMegaPremiumAccount;
+
+// !userLoggedIn;
+// !userHasMegaPremiumAccount;
 
 
 
@@ -20,13 +65,13 @@ userLoggedIn || userHasMegaPremiumAccount;
 //     console.log("Intruder alert!");
 // }
 
-const numberOfSeats = 30;
-let numberOfGuests = 30;
+// const numberOfSeats = 30;
+// let numberOfGuests = 30;
 
-if (numberOfGuests == numberOfSeats){
-    console.log("All seats are taken");
-} else if (numberOfGuests < numberOfSeats){
-    console.log("Allow more guests!")
-} else {
-    console.log("Don't allow more guests!")
-}
+// if (numberOfGuests == numberOfSeats){
+//     console.log("All seats are taken");
+// } else if (numberOfGuests < numberOfSeats){
+//     console.log("Allow more guests!")
+// } else {
+//     console.log("Don't allow more guests!")
+// }
