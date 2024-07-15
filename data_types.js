819af344d942c9a48edc05ae5f19ed7f362b7580
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-15 10:18:11 +0300

    primitives- numbers, boolean, strings

---
+++ data_types.js
@@ -0,0 +1,27 @@
+//-------------primitives-------------
+////------floating-point arithmetic---
+
+// let integerCalculation = 1+2;
+
+// let weirdCalculation = 0.1 + 0.2;
+
+// console.log(weirdCalculation); //0.30000000000000004
+
+// let userisSignedIn = true;
+// let userAdmin = false;
+
+// console.log(userisSignedIn);
+// console.log(userAdmin);
+
+let firstName = "Abby";
+let secondName = "Nduta";
+
+// console.log(firstName);
+// console.log(secondName);
+
+// concatenation
+
+let fullName = firstName + ' ' + secondName;
+console.log(fullName);
+
+
