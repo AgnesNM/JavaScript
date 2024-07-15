f118d81555d18435b5a5d1130c1f1d09c6334569
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-15 10:06:55 +0300

    vars declaration, initialization, reassigning, constants, arithmetic operators

---
+++ vars.js
@@ -0,0 +1,52 @@
+// -------------Addition----------------
+// let totalCDs = 67;
+// let totalVinyls = 34;
+
+// let totalMusic = totalCDs + totalVinyls;
+
+// console.log(totalMusic);
+
+//----------------Subtraction---------------
+
+// let cookiesInJar = 10;
+// let cookiesRemoved = 2;
+
+// let cookiesLeftInJar = cookiesInJar - cookiesRemoved;
+// console.log(cookiesLeftInJar);
+
+//----------(+=)-----(-=)----(++)-----(--)----
+// let cookiesInJar = 10;
+
+// cookiesInJar -= 2; // 8
+
+// cookiesInJar += 12; // 20
+
+// let numberOfLikes = 10;
+
+// numberOfLikes++; //11
+
+// numberOfLikes--; //10
+
+//--------Multiplication-------Division------
+
+// let costPerProduct = 20;
+// let numberOfProducts = 5;
+
+// let totalCost = costPerProduct * numberOfProducts;
+
+// console.log(totalCost); // 100
+
+// let avgCostPerProduct = totalCost / numberOfProducts;
+// console.log(avgCostPerProduct); // 20
+
+//----(*=)----(/=)-----------------
+
+let numberofCats = 2;
+numberofCats *= 6;
+
+// console.log(numberofCats); //12
+
+console.log(numberofCats /= 3);
+
+
+
